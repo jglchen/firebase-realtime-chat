40c6ac4d04ce9ca4c71d2ea1cb31ceab8902bed0
John Chen <jglchen@gmail.com>
2023-02-11 21:39:25 +0800

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,6 +6,15 @@ export default function Layout({ children}: {children: JSX.Element}) {
            <Head>
              <title>Chat Applications with Firebase Cloud Messaging</title>
              <link rel="icon" href="/favicon.ico" />
+             <meta
+              name="description"
+              content="A real-time chat application with Firebase Cloud Messaging"
+              />
+             <meta name="og:title" content="Chat Applications with Firebase Cloud Messaging" />
+             <meta
+              property="og:description"
+              content="A real-time chat application with Firebase Cloud Messaging"
+              />
            </Head>
            <main>
               {children}
